cde42425e3db6890caf514f2e7b2e35936ab7088
elliotteshively <elliotteshively@gmail.com>
2026-06-04 17:46:08 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from runTests11 import *
+from runTests import *
 from inititalSumsMMP import sums
 
 initialSums = sums(11,True) #makes condensing possible 
 jumpValues = initialSums.condense(616000,[],None) #condenses, returns jump values (if want to manually check)
 needsCorollarysMax = min(jumpValues)-1 #sets the value for which above, all values have reciprical sum less than 1
-needsCorollarysList = list(range(10,needsCorollarysMax)) #sets the list to check using corollarys and lemmas (Klein proved for m=7,8,9,10 for maxModuliBound=11)
+needsCorollarysList = list(range(2,needsCorollarysMax)) #checks all m values, by checking from 2 up, some low m's will result in "not solved", but are easily solved by hand (for MMB=11, Klein proved up to m=10)
 
-checkListOutput = runChecklist(needsCorollarysList,True) #runs corollarys and lemmas with runM corollary/lemma order
-runStrings(checkListOutput) #prints counts and m values that needed each step
+checkListOutput = runChecklist(needsCorollarysList,11,True) #runs corollarys and lemmas with runM corollary/lemma order
+runStrings(checkListOutput) #prints counts and m values that needed each step   
---
+++ runTests.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+from utilityMMP import *
+from inititalSumsMMP import *
+from corollarys import *
+from lemmaTen import *
+
+maxModuliBound = 11
+          
+#returns string
+#arguments: int m (minimum modulus), bool printB (should we print the process to console)
+#first generates moduli from m to maxModuliBound*11
+#runs corollary 7 once, then corolalry 8 once, then corollary 9 once, then lemma 10 general placement with p the max prime factor of moduli, then lemma 10 cbp with p the max prime factor of moduli. This suffices for maxModuliBound = 11
+#returns string of form m.TPB where T is the test code (i.e. 7 for corollary 7, 10CBP for lemma 10 critical bin procedure), P is "wpq" (with prime q), q the prime used for that test (if needed), and b is either "T" or "F"
+def runM(m,maxModuliBound,printB):
+    if printB:
+        print()
+        print("Running checks on m =",m)
+    pathString = str(m) + "."
+    moduli = list(range(m,m*maxModuliBound+1))
+
+    pathString += "7"
+    moduli = cor7(moduli,True)
+    if recipricalSum(moduli) < 1:
+        if printB:
+            print("Reciprical sum of moduli is",recipricalSum(moduli))
+        return pathString + "T"
+    
+    pathString += "F.8"
+    moduli = cor8(moduli,True)
+    if recipricalSum(moduli) < 1:
+        if printB:
+            print("Reciprical sum of moduli is",recipricalSum(moduli))
+        return pathString + "T"
+    
+    pathString += "F.9"
+    moduli = cor9(moduli,True)
+    if recipricalSum(moduli) < 1:
+        if printB:
+            print("Reciprical sum of moduli is",recipricalSum(moduli))
+        return pathString + "T"
+    
+    p = maxPrimeFactor(moduli)
+    pathString += "F.10GDwp" + str(p)
+    if lemma10PlaceP(moduli,p,True):
+        return pathString + "T"
+    
+    pathString += "F.10CBPwfp" + str(p) + lemma10CBPString(moduli,p,True)
+    return pathString
+
+#returns list of strings
+#arguments: int list indexList (all indices to check), bool printB (should we print the process to console)
+#calls runM for each m in indexList
+#returns list of all results strings       
+def runChecklist(indexList,maxModuliBound,printB):
+    results = []
+    for i in indexList:
+        results.append(runM(i,maxModuliBound,printB))
+
+    for r in results:
+        print(r)
+    return(results)
+
+#returns void
+#argument: string list stringList (output from runChecklist)
+#parses strings assuming order of application of corollarys/lemmas used by runM
+#prints counts of m's that needed each step
+#prints m values that needed every step
+def runStrings(stringList):
+    sevenCount = 0
+    eightCount= 0
+    nineCount= 0
+    tenCount = 0
+    CBPCount = 0
+    falseCount = 0
+
+    solvedBy789 = []
+    notSolvedBy789 = []
+    solvedBy10 = []
+    solvedByCBP = []
+    notSolved = []
+
+    for s in stringList:
+        if "7T" in s: #if seven enough
+            sevenCount += 1
+            solvedBy789.append(s.split(".")[0])
+        if "8T" in s: #if needed 8, enough
+            eightCount += 1
+            solvedBy789.append(s.split(".")[0])
+        if "9T" in s: #if needed 9, enough
+            nineCount += 1
+            solvedBy789.append(s.split(".")[0])
+        if "7F.8F.9F" in s:
+            notSolvedBy789.append(s.split(".")[0])
+            if not ("CBP" in s):
+                tenCount += 1
+                solvedBy10.append(s.split(".")[0])
+            elif s[-1] == 'T':
+                CBPCount += 1
+                solvedByCBP.append(s.split(".")[0])
+            else:
+                falseCount += 1
+                notSolved.append(s.split(".")[0])
+
+
+
+    print(sevenCount,"true after seven")
+    print(eightCount,"true after eight")
+    print(nineCount,"true after nine")
+    print(tenCount,"true after gen placement")
+    print(CBPCount,"true after CBP")
+    print(falseCount,"unsolved")
+    print("These m vals needed lemma 10:",notSolvedBy789)
+    print("These m vals needed lemma 10, solved by lemma 10 first app:",solvedBy10)
+    print("These m vals needed CBP, solved by CBP (lemma 10 second app):",solvedByCBP)
+    print("These not solved:",notSolved)
+            
+
+
+
