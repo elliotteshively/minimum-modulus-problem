f4e24f364fd996f0ed9ed55157a4e69b62240d18
elliotteshively <elliotteshively@gmail.com>
2026-06-04 17:49:33 -0600

    Fix import statements and adjust needsCorollarysMax

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from inititalSumsMMP import sums
 
 initialSums = sums(11,True) #makes condensing possible 
 jumpValues = initialSums.condense(616000,[],None) #condenses, returns jump values (if want to manually check)
-needsCorollarysMax = min(jumpValues)-1 #sets the value for which above, all values have reciprical sum less than 1
+needsCorollarysMax = min(jumpValues) #sets the value for which above, all values have reciprical sum less than 1
 needsCorollarysList = list(range(2,needsCorollarysMax)) #checks all m values, by checking from 2 up, some low m's will result in "not solved", but are easily solved by hand (for MMB=11, Klein proved up to m=10)
 
 checkListOutput = runChecklist(needsCorollarysList,11,True) #runs corollarys and lemmas with runM corollary/lemma order
